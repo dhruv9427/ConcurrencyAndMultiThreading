0a7d73e723608890ae76d313282455151023904a
Dhruv Singh <dhruv_9427@hotmail.com>
2017-05-24 23:30:42 +0100

    Adding stuff
    Changing Diva Thread to DKS Thread

--- src/Demo.java
+++ src/Demo.java
@@ -5,7 +5,9 @@ public class Demo {
     public static void main(String[] args) {
         Thread t = Thread.currentThread();
         System.out.println("Current thread: " + t);
-        t.setName("Diva Thread");
+        t.setName("DKS Thread");
         System.out.println("Renamed thread: " + t);
+        System.out.println("GitHub Changes");
+        System.out.println("Trying this again just to make sure I am confident");
     }
 }
