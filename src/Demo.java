e5ed7031b8b78b6133aaa47912931831cc56e336
Dhruv Singh <dhruv_9427@hotmail.com>
2017-05-24 23:03:01 +0100

    Changed Dhruv Thread to Diva Thread

--- src/Demo.java
+++ src/Demo.java
@@ -2,4 +2,10 @@
  * Created by DhruvSingh on 24/05/2017.
  */
 public class Demo {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        System.out.println("Current thread: " + t);
+        t.setName("Diva Thread");
+        System.out.println("Renamed thread: " + t);
+    }
 }
